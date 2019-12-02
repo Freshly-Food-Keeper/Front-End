60a834c087e78e0d01feb63c08b45de5edb1d2b2
Meredyth Houpos <meredyth.houpos@gmail.com>
2019-12-02 14:21:55 -0500

    Updated Nutrition component

---
+++ store/reducers/nutrition.js
@@ -0,0 +1,104 @@
+import axios from 'axios'
+import {
+  NUTRITIONX_APPKEY,
+  NUTRITIONX_APPID
+} from '../../config/secrets.js'
+
+const nutrition = []
+
+const GOT_NUTRITION = 'GOT_NUTRITION'
+
+const gotNutrition = info => ({
+  type: GOT_NUTRITION,
+  info
+})
+
+const createNutritionArray = info => ([
+  {
+    value: info.nf_calories,
+    label: 'Calories',
+    header: true
+  },
+  {
+    value: info.nf_total_fat,
+    label: 'Total Fat (g)',
+    header: true
+  },
+  {
+    value: info.nf_saturated_fat,
+    label: 'Saturated Fat (g)',
+    header: false
+  },
+  {
+    value: info.nf_cholesterol,
+    label: 'Cholesterol',
+    header: true
+  },
+  {
+    value: info.nf_sodium,
+    label: 'Sodium',
+    header: true
+  },
+  {
+    value: info.nf_total_carbohydrate,
+    label: 'Carbohydrates',
+    header: true
+  },
+  {
+    value: info.nf_dietary_fiber,
+    label: 'Fiber',
+    header: false
+  },
+  {
+    value: info.nf_sugars,
+    label: 'Sugars',
+    header: false
+  },
+  {
+    value: info.nf_protein,
+    label: 'Protein',
+    header: true
+  },
+  {
+    value: info.nf_vitamin_a_dv,
+    label: 'Vitamin A',
+    header: true
+  },
+  {
+    value: info.nf_vitamin_c_dv,
+    label: 'Vitamin C',
+    header: true
+  },
+  {
+    value: info.nf_iron_dv,
+    label: 'Iron',
+    header: true
+  },
+  {
+    value: info.nf_potassium,
+    label: 'Potassium',
+    header: true
+  }
+])
+
+export const getNutrition = food => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(
+      `https://api.nutritionix.com/v1_1/search/${food}?results=0:20&fields=brand_name,item_name,brand_id,item_id,upc,item_description,nf_ingredient_statement,nf_calories,nf_total_fat,nf_saturated_fat,nf_cholesterol,nf_sodium,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_vitamin_a_dv,nf_vitamin_c_dv,nf_calcium_dv,nf_iron_dv,nf_potassium,nf_servings_per_container,nf_serving_size_unit,nf_serving_weight_grams,images_front_full_url&appId=${NUTRITIONX_APPID}&appKey=${NUTRITIONX_APPKEY}`
+    )
+      dispatch(gotNutrition(createNutritionArray(data.hits[0].fields)))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export default function(state = nutrition, action) {
+  switch (action.type) {
+    case GOT_NUTRITION:
+      return action.info
+    default:
+      return state
+  }
+}
